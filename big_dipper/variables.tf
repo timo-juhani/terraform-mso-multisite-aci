3a289b1e07be76a768ca6b31c9f5d0da96c483a9
timo-juhani <timo.juhani.karjalainen@protonmail.com>
2020-12-02 10:26:59 +0200

    create big_dipper example, do folder clean-up

---
+++ big_dipper/variables.tf
@@ -0,0 +1,80 @@
+# MSO crendentials
+variable "username" {
+  default = "admin"
+}
+variable "password" {
+  default = "SanFran1234!"
+}
+variable "url" {
+  default = "https://10.1.106.35/"
+}
+
+# Site IDs can be found using the Swagger documentation at MSO
+variable "site1_id" {
+  default = "5f9ac0371100001703b9d6c0"
+}
+
+# One tenant to map all application components in the same place
+variable "tenant" {
+  default = "big-dipper"
+}
+# Single schema that consists site specific and intersite objects
+variable "schema" {
+  default = "ms-big-dipper"
+}
+variable "intersite_template" {
+  default = "big-dipper-intersite"
+}
+variable "site1_template" {
+  default = "big-dipper-site1"
+}
+variable "site2_template" {
+  default = "big-dipper-site2"
+}
+
+# Three APs and three EPGs are required for this example tenant
+variable "ap_intersite" {
+  default = "big-dipper-intersite"
+}
+variable "ap_site1" {
+  default = "big-dipper-site1"
+}
+variable "ap_site2" {
+  default = "big-dipper-site2"
+}
+variable "epg_intersite" {
+  default = "big-dipper-GW"
+}
+variable "epg_site1" {
+  default = "big-dipper-VM-site1"
+}
+variable "epg_site2" {
+  default = "big-dipper-VM-site2"
+}
+
+# A single VRF is allocated that connects all BDs
+variable "vrf_intersite" {
+  default = "big-dipper-intersite"
+}
+
+# A BD per EPG is created
+variable "bd_intersite" {
+  default = "big-dipper-GW"
+}
+variable "bd_site1" {
+  default = "big-dipper-VM-site1"
+}
+variable "bd_site2" {
+  default = "big-dipper-VM-site2"
+}
+
+# Contracts are used to allow traffic between EPGs
+variable "c_permit_all_site1" {
+  default = "permit-all-site1"
+}
+variable "c_permit_all_site2" {
+  default = "permit-all-site2"
+}
+variable "f_permit_all" {
+  default = "permit-all"
+}
